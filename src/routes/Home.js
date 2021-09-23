5e38f33eddd8ac6c43f20427985bf89ad582c4b3
lelemita <nywfox@gmail.com>
2021-09-23 22:06:46 +0900

    Read Database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,24 @@
 import { dbService } from "fbase";
-import React, {useState} from "react";
-import { collection, addDoc } from "firebase/firestore"; 
+import React, {useState, useEffect} from "react";
+import { collection, addDoc, getDocs } from "firebase/firestore"; 
 
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(dbService, "nweets"));
+        dbNweets.forEach((doc) => {
+            const nweetObj = {
+                ...doc.data(),
+                id: doc.id,
+            }
+            setNweets(prev => [nweetObj, ...prev])
+        })
+    };
+    useEffect(() => {
+        getNweets();
+    }, [])
     const onSubmit = async (event) => {
         event.preventDefault();
         await addDoc(collection(dbService, "nweets"), {
@@ -23,6 +37,13 @@ const Home = () => {
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120}/>
                 <input type="submit" value="Nweet" />
             </form>
+            <div>
+                {nweets.map((nweet) => (
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
