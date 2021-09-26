49974fc7d7dce3b81d5fa8d51795b068020aef76
lelemita <nywfox@gmail.com>
2021-09-26 13:43:13 +0900

    MOD Home order by CreatedAt

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,28 @@
 import { dbService } from "fbase";
 import React, {useState, useEffect} from "react";
-import { collection, onSnapshot } from "firebase/firestore";
+import { collection, getDocs, query, orderBy } from "firebase/firestore";
 import Nweet from "components/Nweet";
 import NweetFactory from "components/NweetFactory";
 
+
 const Home = ({ userObj }) => {
     const [nweets, setNweets] = useState([]);
+    const getAllNweets = async () => {
+        const qry = query(
+            collection(dbService, "nweets"),
+            orderBy("createdAt", "desc")
+        );
+        const snap = await getDocs(qry);
+        const nweetArr = snap.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+        setNweets(nweetArr)
+    };
     useEffect(() => {
-        onSnapshot(collection(dbService, "nweets"), (snap) => {
-            const nweetArr = snap.docs.map((doc) => ({
-                id: doc.id,
-                ...doc.data()
-            }))
-            setNweets(nweetArr);
-        });
+        getAllNweets();
     }, [])
+
     return(
         <div className="container">
             <NweetFactory userObj={userObj} />
